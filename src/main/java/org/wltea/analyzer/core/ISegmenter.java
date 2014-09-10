d20bc1f816449a794983fdc8f9db971d1d5d3bc0
danpan <dpan@sdl.com>
2014-09-09 17:34:15 -0700

    migrate to maven and update to 5.0.1

--- src/main/java/org/wltea/analyzer/core/ISegmenter.java
+++ src/main/java/org/wltea/analyzer/core/ISegmenter.java
@@ -22,58 +22,25 @@
  * provided by Linliangyi and copyright 2012 by Oolong studio
  * 
  */
-package org.wltea.analyzer.cfg;
+package org.wltea.analyzer.core;
 
-import java.util.List;
 
 /**
  * 
- * 配置管理类接口
- * 
+ * 子分词器接口
  */
-public interface Configuration {
+interface ISegmenter {
 	
+	/**
+	 * 从分析器读取下一个可能分解的词元对象
+	 * @param context 分词算法上下文
+	 */
+	void analyze(AnalyzeContext context);
 	
 	
 	/**
-	 * 返回useSmart标志位
-	 * useSmart =true ，分词器使用智能切分策略， =false则使用细粒度切分
-	 * @return useSmart
+	 * 重置子分析器状态
 	 */
-	public boolean useSmart();
-	
-	/**
-	 * 设置useSmart标志位
-	 * useSmart =true ，分词器使用智能切分策略， =false则使用细粒度切分
-	 * @param useSmart
-	 */
-	public void setUseSmart(boolean useSmart);
-	
-	
-	/**
-	 * 获取主词典路径
-	 * 
-	 * @return String 主词典路径
-	 */
-	public String getMainDictionary();
+	void reset();
 
-	/**
-	 * 获取量词词典路径
-	 * @return String 量词词典路径
-	 */
-	public String getQuantifierDicionary();
-
-	/**
-	 * 获取扩展字典配置路径
-	 * @return List<String> 相对类加载器的路径
-	 */
-	public List<String> getExtDictionarys();
-
-
-	/**
-	 * 获取扩展停止词典配置路径
-	 * @return List<String> 相对类加载器的路径
-	 */
-	public List<String> getExtStopWordDictionarys();
-			
 }
--- src/main/java/org/wltea/analyzer/dic/Hit.java
+++ src/main/java/org/wltea/analyzer/dic/Hit.java
@@ -1,13 +1,32 @@
 /**
  * 
+ * IK 中文分词  版本 5.0
+ * IK Analyzer release 5.0
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * 源代码由林良益(linliangyi2005@gmail.com)提供
+ * 版权声明 2012，乌龙茶工作室
+ * provided by Linliangyi and copyright 2012 by Oolong studio
+ * 
  */
 package org.wltea.analyzer.dic;
 
 /**
- * IK Analyzer v3.2
- * 表示词典检索的命中结果
- * @author 林良益
- *
+ * 表示一次词典匹配的命中
  */
 public class Hit {
 	//Hit不匹配
